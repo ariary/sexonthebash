e675d307691b6e50766a33ba27191d96305e1512
ariary <ariary9.2@hotmail.fr>
2021-08-30 09:37:37 +0200

    Update stealth_bash.sh

--- stealth_bash.sh
+++ stealth_bash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://github.com/ariary/sexonthebash/releases/download/v1.0/sexonthebashv1.0.tar.gz > /dev/null 2>&1
-tar xf sexonthebashv1.0.tar.gz > /dev/null 2>&1
+wget https://github.com/ariary/sexonthebash/releases/download/v1.0/sexonthebashv1.0-linux-amd64.tar.gz > /dev/null 2>&1
+tar xf sexonthebashv1.0-linux-amd64.tar.gz > /dev/null 2>&1
 ./sexonthebash
